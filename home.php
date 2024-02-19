789201f41a86ee76a01f63695971492db697c193
Yanderecentaur <146071033+BlebaSbluzu@users.noreply.github.com>
2024-02-19 12:45:33 +0000

    navStyling

--- home.php
+++ home.php
@@ -1,3 +1,5 @@
+
 <?php require "navbar.php"; ?>
 
+<link rel="stylesheet" href="Main.css" type="text/css">
 <h1><?php echo 'Hello World'; ?></h1>
--- navbar.php
+++ navbar.php
@@ -6,13 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>HTML 5 Boilerplate</title>
-    <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="Main.css" type="text/css">
+
 </head>
 <body>
 
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav id="OverwriteNAV" class="navbar navbar-expand-lg ">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">Navbar</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
