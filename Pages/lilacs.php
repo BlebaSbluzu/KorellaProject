509295e843b096f602a5d4c889c8e70cabebf8c1
Jernny <145683524+Rosaleenvdwel@users.noreply.github.com>
2024-03-13 21:08:55 +0000

    update 1

--- Pages/lilacs.php
+++ Pages/lilacs.php
@@ -1,6 +1,17 @@
 <?php require "../Layout/navbar.php"; ?>
 
 <link rel="stylesheet" href="../CSS/Main.css" type="text/css">
+<link rel="stylesheet" href="../CSS/Products.css" type="text/css">
 
-<img src="../Images/Lilacs(Spring).jpg" width="300" height="300">
+<div class="product-container">
+    <img src="../Images/Lilacs(Spring).jpg" class="product-image" width="300" height="300">
+    <div class="product-info">
+        <h1>Pink Lilac</h1>
+        <p>A stunning addition to any garden! With its blush-pink blossoms and sweet fragrance,</p>
+        <p>this shrub is sure to enchant. Perfect for borders or standalone beauty,</p>
+        <p>it thrives in various conditions. Add a touch of springtime romance to your garden today!</p>
+    </div>
+</div>
+
+<img src="../Images/lilac%20pruning.jpg">
 
