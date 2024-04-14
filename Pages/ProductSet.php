61f8447474d58e1dab7296318ad221697875e93f
Yanderecentaur <146071033+BlebaSbluzu@users.noreply.github.com>
2024-04-14 23:34:20 +0100

    products db connection and object!
    Made a quick and easy way to produce products on the page with a productset class
    Reads the products from the database.
    Also fixed up the images being chosen from the database aswell

---
+++ Pages/ProductSet.php
@@ -0,0 +1,98 @@
+<?php
+
+class ProductSet
+{
+
+    public $title;
+    public $sql;
+    public $ItemsAmount;
+
+    /**
+     * @param $title
+     * @param $sql
+     * @param $ItemsAmount
+     */
+    public function __construct($title, $sql)
+    {
+        $this->title = $title;
+        $this->sql = $sql;
+
+
+        MakeSet($title, $sql);
+
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+
+}
+
+function MakeSet($title, $sql)
+{
+include '../src/DBconnect.php';
+
+$statement = $connection->prepare($sql);
+        $statement->execute();
+        $result = $statement->fetchAll();
+    ?>
+
+<link rel="stylesheet" href="../CSS/Main.css" type="text/css">
+
+<link rel="stylesheet" href="../CSS/Card.css" type="text/css">
+
+
+    <div class="Titles">
+        <hr class="hrs">
+        <h3 class="sacramento-regular"> <?php echo $title ?> </h3>
+        <hr class="hrs"></div>
+<div id="cardRow" class="card-group cardRow">
+    <?php
+
+//    for($i=0;$i<=$ItemsAmount;$i++) {
+            foreach ($result as $product) {
+//                echo escape($product["image"]);
+//                $imageName = escape($product["image"]);
+//                echo $imageName;
+
+    ?>
+<!--                src="/images/--><?php //echo $cutePet['image']; ?><!--"-->
+
+
+
+    <div id="cardStyles" class="card">
+
+
+        <img src="../Images/<?php echo $product["image"]; ?>" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title"><?php echo $product["ProductName"]; ?></h5>
+        </div>
+        <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+    </div>
+
+
+
+<?php
+
+    }
+}
+
+
+//public  function getSql() {
+//    return $this->sql;
+//}
+//    function __construct($title, $sql){
+//        $this->title = $title;
+//        $this->sql = $sql;
+//    }
+//public $statement = $connection->prepare($sql);
+//$statement->execute();
+//$result = $statement->fetchAll();
+//}
+
+?>
+    </div>
--- Pages/home.php
+++ Pages/home.php
@@ -1,4 +1,3 @@
-
 <?php
 
 //try {
@@ -57,56 +56,75 @@ require '../src/DBconnect.php';
     </button>
 </div>
 
-<?php require "./Card.php" ?>
-
-<?php //include "./Cards.php" ;
-
-$bestSellers = new Card("Recommended");
+<?php include "./ProductSet.php";
+require "../common.php";
 
 
+
+$homeRow1 = new ProductSet("Popular Items","Select * from Products limit 3;");
+$homeRow2 = new ProductSet("Popular Items","Select * from Products limit 3;");
+$homeRow3 = new ProductSet("Popular Items","Select * from Products limit 3;");
 
 ?>
 
 
-<div class="Titles">
-    <hr class="hrs">
-    <h3 class="sacramento-regular"> <?php echo $bestSellers->getTitle(); ?> </h3>
-    <hr class="hrs">
 
+
+
+<!--<div class="Titles">-->
+<!--    <hr class="hrs">-->
+<!--    <h3 class="sacramento-regular"> --><?php //echo $bestSellers->getTitle(); ?><!-- </h3>-->
+<!--    <hr class="hrs">-->
+<!---->
+<!--</div>-->
+<?php
+//if ($result && $statement->rowCount() > 0) {
+?>
+
+
+<!---->
+<!--<link rel="stylesheet" href="../CSS/Main.css" type="text/css">-->
+<!---->
+<!--<link rel="stylesheet" href="../CSS/Card.css" type="text/css">-->
+<!--<div id="cardRow" class="card-group">-->
+
+
+
+
+<!--    --><?php //foreach ($result as $item) { ?>
+<!--    <div id="cardStyles" class="card">-->
+<!--        <img src="..." class="card-img-top" alt="...">-->
+<!--        <div class="card-body">-->
+<!--            <h5 class="card-title">Card title</h5>-->
+<!--            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>-->
+<!--        </div>-->
+<!--        <div class="card-footer">-->
+<!--            <small class="text-muted">Last updated 3 mins ago</small>-->
+<!--        </div>-->
+<!--    </div>-->
+
+
+<!--    <div id="cardStyles" class="card">-->
+<!--        <img src="..." class="card-img-top" alt="...">-->
+<!--        <div class="card-body">-->
+<!--            <h5 class="card-title">Card title</h5>-->
+<!--            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>-->
+<!--        </div>-->
+<!--        <div class="card-footer">-->
+<!--            <small class="text-muted">Last updated 3 mins ago</small>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--    <div id="cardStyles" class="card">-->
+<!--        <img src="..." class="card-img-top" alt="...">-->
+<!--        <div class="card-body">-->
+<!--            <h5 class="card-title">Card title</h5>-->
+<!--            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>-->
+<!--        </div>-->
+<!--        <div class="card-footer">-->
+<!--            <small class="text-muted">Last updated 3 mins ago</small>-->
+<!--        </div>-->
+<!--    </div>-->
 </div>
-<link rel="stylesheet" href="../CSS/Main.css" type="text/css">
-
-<link rel="stylesheet" href="../CSS/Card.css" type="text/css">
-<div id="cardRow" class="card-group">
-    <div id="cardStyles" class="card">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
-        <div class="card-footer">
-            <small class="text-muted">Last updated 3 mins ago</small>
-        </div>
-    </div>
-    <div id="cardStyles" class="card">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-        </div>
-        <div class="card-footer">
-            <small class="text-muted">Last updated 3 mins ago</small>
-        </div>
-    </div>
-    <div id="cardStyles" class="card">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-        </div>
-        <div class="card-footer">
-            <small class="text-muted">Last updated 3 mins ago</small>
-        </div>
-    </div>
-</div>
-
+<?php
+//}
+//?>
