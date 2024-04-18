04daafd56fa2887b439ed96cd0b366cdbdacbefa
Yanderecentaur <146071033+BlebaSbluzu@users.noreply.github.com>
2024-04-18 01:28:50 +0100

    Update init.sql

--- data/init.sql
+++ data/init.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE test;
- use test;
+
+ use gardencentredb;
 CREATE TABLE users (
                        id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
                        username VARCHAR(30) NOT NULL,
