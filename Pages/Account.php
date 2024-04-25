93ddceedd4ad87a8d2ebcdbd9778ecdc63843c83
Yanderecentaur <146071033+BlebaSbluzu@users.noreply.github.com>
2024-04-25 05:46:48 +0100

    Create Account.php

---
+++ Pages/Account.php
@@ -0,0 +1,14 @@
+
+<?php include '../Layout/navbar.php'; ?>
+<?php require_once('../template/header.php');
+require '../src/DBconnect.php';
+
+
+?>
+
+<p>Welcome <?php echo $_SESSION['Username'] ?> </p>
+
+
+
+<?php
+
