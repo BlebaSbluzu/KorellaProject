d8b89324dcd1ea4e0d40176dca59df0639808ece
Jernny <145683524+Rosaleenvdwel@users.noreply.github.com>
2024-04-25 03:18:43 +0100

    guidance testing

--- Pages/seasonal.php
+++ Pages/seasonal.php
@@ -1,13 +1,47 @@
 <?php require "../Layout/navbar.php";
 include "./ProductSet.php";?>
 
+<?php
+//season will appear based on current month ref: function.date.php
+$month = date('n');
 
+//uncomment for our guidance testing
+//$month = 4; // Spring: April
+// $month = 7; // Summer: July
+//$month = 10; // Autumn: October
+// $month = 1; // Winter: January
+
+//determines each season by the month starting with winter
+$season = 'Winter';
+if ($month >= 3 && $month <= 5) {
+$season = 'Spring';
+} elseif ($month >= 6 && $month <= 8) {
+$season = 'Summer';
+} elseif ($month >= 9 && $month <= 11) {
+$season = 'Autumn';
+}
+
+//seasonal image change
+//formats the season text with image ref: function.strtolower.php
+$backgroundImage = strtolower($season) . "-header.jpg";
+
+?>
+
+<!--styled here so the php can interacte with our html-->
+<style>
+    .cardRow {
+        background: url('../Images/<?= $backgroundImage ?>') no-repeat center center;
+    }
+</style>
 <link rel="stylesheet" href="../CSS/Main.css" type="text/css">
 <link rel="stylesheet" href="../CSS/seasonal.css" type="text/css">
 
-<!--<h2>Spring</h2>-->
+//current season will be printed
 <div id="cardRow" class=" cardRow ">
-
+    <div class="season-greeting">
+        <h1 >It's <?= htmlspecialchars($season)?>!</h1>
+    </div>
+</div>
 <?php
 
 $Spring = new ProductSet("Spring", "SELECT * from product where SEASON = 'Spring' ");
